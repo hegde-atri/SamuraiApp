e5d55f4fe3d7db7c579b48e81c5d0cb8c040124a
hegde-atri <iamatrihegde@gmail.com>
2021-07-07 21:03:08 +0100

    switching machines

--- SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -12,5 +12,10 @@ namespace SamuraiApp.Data
     {
         public DbSet<Samurai> Samurais { get; set; }
         public DbSet<Quote> Quotes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source= (localdb); Initial Catalog=SamuraiAppData");
+        }
     }
 }
